c1db8cd6dcb5869270c7597549b6971baf0b9865
bqanderson <bqanderson@me.com>
2020-01-24 21:59:07 -0600

    Add base styles, create header component.

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,42 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby'
+import React from 'react'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+import HeaderLogo from '../../static/images/logos/bqa-logo.svg'
+
+const Header = () => (
+  <header>
+    <div className='header-row'>
+      <div className='logo-wrapper'>
+        <Link to='/'>
+          <HeaderLogo height='48' width='143' />
         </Link>
-      </h1>
+      </div>
+      <nav>
+        <ul>
+          <li className='menu-item'>
+            <Link className='nav-link' to='/page-2/'>
+              Projects
+            </Link>
+          </li>
+          <li className='menu-item'>
+            <Link className='nav-link' to='/page-2/'>
+              About
+            </Link>
+          </li>
+          <li className='menu-item'>
+            <Link className='nav-link' to='/page-2/'>
+              Experience
+            </Link>
+          </li>
+          <li className='menu-item'>
+            <Link className='nav-link' to='/page-2/'>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
